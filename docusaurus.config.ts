498bcbafadcba00cc71c24eaf11f4417f2602142
thomasjwilliam <thomas.j.william@gmail.com>
2024-05-23 17:42:49 +0200

    use hai favicon

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,7 +5,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Hai',
   tagline: 'documentation',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Set the production url of your site here
   url: 'https://github.com',
