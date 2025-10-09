99f7948eaba0486debea2032be6692e4a81be1cb
txvdv <txvdv@protonmail.com>
2025-10-09 13:21:10 +0200

    Update title to HAI in topbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Hai',
+      title: 'HAI',
       // logo: {
       //   alt: 'My Site Logo',
       //   src: 'img/logo.svg',
